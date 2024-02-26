3f9d119d290e73fc6e5dcfffd62323d83de35f3c
skrydg <skrrydg@yandex.ru>
2024-02-26 22:07:16 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/drop_columns.py
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/drop_columns.py
@@ -7,30 +7,33 @@ class DropColumnsStep:
     def __init__(self):
         self.columns = []
         
-    def process_train_dataset(self, df):
-        self.
-        for column in df.columns:
-            isnull = df[column].is_null().mean()
-            if isnull > 0.95:
-                self.columns.append(column)
-
-        for column in df.columns:
-            if df[column].dtype == pl.Enum:
-                freq = df[column].n_unique()
-
-                if (freq == 1) or (freq > 200):
+    def process_train_dataset(self, dataset):
+        for name, table in dataset.get_tables():
+            for column in table.columns:
+                isnull = table[column].is_null().mean()
+                if isnull > 0.95:
                     self.columns.append(column)
+
+            for column in table.columns:
+                if table[column].dtype == pl.Enum:
+                    freq = table[column].n_unique()
+
+                    if (freq == 1) or (freq > 200):
+                        self.columns.append(column)
 
         self.columns.append("date_decision")
         self.columns.append("MONTH")
                 
         print("Columns to drop: {}".format(self.columns))            
-        return self.process(df)
+        return self.process(dataset)
         
-    def process_test_dataset(self, df):
-        return self.process(df)
+    def process_test_dataset(self, dataset):
+        return self.process(dataset)
     
-    def _process(self, df):
-        for column in self.columns:
-            df = df.drop(column)
-        return df
+    def _process(self, dataset):
+        assert(type(dataset) is Dataset)
+        for name, table in dataset.get_tables():
+            for column in self.columns:
+                table = table.drop(column)
+            dataset.set(name, table)
+        return dataset
