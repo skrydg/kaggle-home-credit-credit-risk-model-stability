4916dec068e08060f1c66103fd505d68d8fd0473
skrydg <skrrydg@yandex.ru>
2024-03-15 00:26:12 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/generate_base_date_diff.py
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/generate_base_date_diff.py
@@ -13,8 +13,13 @@ class GenerateBaseDateDiffStep:
         return self.process(df, columns_info)
     
     def process(self, df, columns_info):
-        dates_columns = [column for column in table.columns if "DATE" in columns_info.get_labels(column)]
+        dates_columns = [
+            column 
+            for column in df.columns 
+            if ("DATE" in columns_info.get_labels(column)) and (column != self.base_column)
+        ]
         for column in dates_columns:
             new_column_name = f"{column}_{self.base_column}_diff"
             df = df.with_columns((pl.col(column) - pl.col(self.base_column)).alias(new_column_name))
+        print("Create {} new date diff columns, with base_column={}".format(len(dates_columns), self.base_column))
         return df, columns_info
