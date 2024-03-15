1607549bae33db71a4b31c2126071aa73ff17f73
skrydg <skrrydg@yandex.ru>
2024-03-15 21:35:33 +0400

    improve drop equal columns

--- src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/drop_equal_columns.py
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/drop_equal_columns.py
@@ -6,14 +6,15 @@ from kaggle_home_credit_risk_model_stability.libs.input.dataset import Dataset
 
 class DropEqualColumnsStep:
     def __init__(self):
-        self.columns = []
+        self.columns_to_drop = []
+        self.duplicates = {}
         
     def process_train_dataset(self, dataset, columns_info):
         for name, table in dataset.get_tables():
             self._fill_columns_to_drop(table, columns_info)
             
-        print("Drop {} columns as duplicates".format(len(self.columns)))
-        print(f"Columns to drop: {self.columns}")
+        print("Drop {} columns as duplicates".format(len(self.columns_to_drop)))
+        print(f"Columns to drop: {self.columns_to_drop}")
 
         return self._process(dataset, columns_info)
         
@@ -32,18 +33,33 @@ class DropEqualColumnsStep:
             hash_result.update(str(table[column].dtype).encode('utf-8'))
             hash_result = hash_result.hexdigest()
             if hash_result in unique_columns:
-                self.columns.append(column)
+                self.columns_to_drop.append(column)
                 column1 = column
                 column2 = unique_columns[hash_result]
                 assert(table[column1].equals(table[column2].alias(column1).cast(table[column1].dtype)))
                 columns_info.add_label(column1, f"DUPLICATE_OF_{column2}")
                 columns_info.add_label(column2, f"DUPLICATE_OF_{column1}")
+                self.duplicates[column2].append(column1)
             else:
                 unique_columns[hash_result] = column
+                self.duplicates[column] = []
         
     def _process(self, dataset, columns_info):
-        assert(type(dataset) is Dataset)
+        assert(type(dataset) is Dataset)        
         for name, table in dataset.get_tables():
-            table = table.drop(self.columns)
+            table, columns_info = self._process_table(table, columns_info)
             dataset.set(name, table)
         return dataset, columns_info
+    
+    def _process_table(self, table, columns_info):
+        for column, duplicates in self.duplicates.items():
+            for duplicate in duplicates:
+                if table[column].dtype != table[duplicate].dtype:
+                    continue
+                table = table.with_columns(
+                    pl.when(pl.col(column).is_not_null())
+                        .then(pl.col(column))
+                        .otherwise(pl.col(duplicate))
+                )
+        table = table.drop_columns(self.columns_to_drop)
+        return table, columns_info
