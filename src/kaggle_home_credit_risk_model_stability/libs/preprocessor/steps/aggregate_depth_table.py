5ab0dd194c42cd25b0d3364e0056bbc3841362f3
skrydg <skrrydg@yandex.ru>
2024-03-16 22:20:26 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/aggregate_depth_table.py
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/aggregate_depth_table.py
@@ -5,7 +5,6 @@ from kaggle_home_credit_risk_model_stability.libs.input.dataset import Dataset
 
 class Aggregator:
     num_aggregators = [pl.max, pl.min, pl.first, pl.last, pl.mean, pl.std, pl.var, pl.count]
-    enum_aggregators = [pl.first, pl.last, pl.max, pl.min]
     enum_to_num_aggregators = [pl.n_unique, pl.count]
 
     @staticmethod
@@ -32,35 +31,30 @@ class Aggregator:
         columns = [column for column in df.columns if df[column].dtype == pl.Enum]
         
         expr_all = []
-        for method in Aggregator.enum_aggregators:
-            for column in columns:
-                known_column = df[column].filter(pl.col(column) != "__UNKNOWN__")
-                expr_all.append(
-                    [method(known_column).alias(f"{method.__name__}_{column}") for column in columns]  
-                )
+        for column in columns:
+            filtered_column = pl.col(column).filter(pl.col(column).cast(pl.String) != "__UNKNOWN__")
+            expr_all.extend([
+                filtered_column.max().alias(f"max_{column}"),
+                filtered_column.min().alias(f"min_{column}"),
+                filtered_column.first().alias(f"first_{column}"),
+                filtered_column.last().alias(f"last_{column}"),
+                filtered_column.mode().max().alias(f"mode_{column}")
+            ])
 
-            for column in columns:
-                labels = columns_info.get_labels(column)
-                if "RAW" in labels:
-                    labels.remove("RAW")
-                columns_info.add_labels(f"{method.__name__}_{column}", labels)
+            labels = columns_info.get_labels(column)
+            if "RAW" in labels:
+                labels.remove("RAW")
+            columns_info.add_labels(f"max_{column}", labels)
+            columns_info.add_labels(f"min_{column}", labels)
+            columns_info.add_labels(f"first_{column}", labels)
+            columns_info.add_labels(f"last_{column}", labels)
+            columns_info.add_labels(f"mode_{column}", labels)
         
         for method in Aggregator.enum_to_num_aggregators:
             expr = [method(column).alias(f"{method.__name__}_{column}") for column in columns]
             expr_all += expr
 
-        expr_mode = [
-            pl.col(column)
-            .drop_nulls()
-            .mode()
-            .max() # use max for generate stable result
-            .alias(f"mode_{column}")
-            for column in columns
-        ]
-        for column in columns:
-          columns_info.add_labels(f"mode_{column}", {"CATEGORICAL"})
-
-        return expr_all + expr_mode
+        return expr_all
 
 
     @staticmethod
