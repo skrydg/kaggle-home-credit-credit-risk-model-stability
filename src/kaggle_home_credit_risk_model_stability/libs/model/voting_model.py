090a121a6b78d9f049a8d2cb64aed065c0b1270d
skrydg <skrrydg@yandex.ru>
2024-03-21 13:01:30 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/model/voting_model.py
+++ src/kaggle_home_credit_risk_model_stability/libs/model/voting_model.py
@@ -3,6 +3,20 @@ import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
 
 class VotingModel(BaseEstimator, RegressorMixin):
+    def __init__(self, estimators):
+        super().__init__()
+        self.estimators = estimators
+    
+    def predict(self, X):
+        y_preds = [estimator.predict(X) for estimator in self.estimators]
+        return np.mean(y_preds, axis=0)
+    
+    def predict_proba(self, X):
+        y_preds = [estimator.predict_proba(X) for estimator in self.estimators]
+        return np.mean(y_preds, axis=0)
+  
+
+class WeightVotingModel(BaseEstimator, RegressorMixin):
     def __init__(self, estimators, w = None):
         super().__init__()
         self.estimators = np.array(estimators)
