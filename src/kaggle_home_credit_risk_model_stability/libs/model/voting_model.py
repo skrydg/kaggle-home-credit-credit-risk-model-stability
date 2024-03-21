a9ea1ce43c70e6b4d60c163228db2a6cf33e98b1
skrydg <skrrydg@yandex.ru>
2024-03-21 12:55:00 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/model/voting_model.py
+++ src/kaggle_home_credit_risk_model_stability/libs/model/voting_model.py
@@ -3,15 +3,16 @@ import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
 
 class VotingModel(BaseEstimator, RegressorMixin):
-    def __init__(self, estimators):
+    def __init__(self, estimators, w = None):
         super().__init__()
-        self.estimators = estimators
+        self.estimators = np.array(estimators)
+        if w is None:
+            self.w = np.ones(self.estimators.shape)
+        else:
+            self.w = w
+        self.w = self.w / np.sum(self.w)
     
     def predict(self, X):
-        y_preds = [estimator.predict(X) for estimator in self.estimators]
-        return np.mean(y_preds, axis=0)
-    
-    def predict_proba(self, X):
-        y_preds = [estimator.predict_proba(X) for estimator in self.estimators]
-        return np.mean(y_preds, axis=0)
+        y_preds = np.array([estimator.predict(X) for estimator in self.estimators])
+        return np.sum(y_preds * self.w, axis=0)
   
