9980da5f00f4d6ac2197e43b1aa86830f5f3e935
skrydg <skrrydg@yandex.ru>
2024-03-23 22:44:10 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/create_day_feature.py
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/create_day_feature.py
@@ -12,8 +12,7 @@ class CreateDayFeatureStep:
     
     def process(self, dataset, columns_info):
         base = dataset.get_base()
-        base = 
-        gibase.with_columns(
+        base = gbase.with_columns(
             month_decision = pl.col("date_decision").cast(pl.Date).dt.month(),
             weekday_decision = pl.col("date_decision").cast(pl.Date).dt.weekday(),
         )
