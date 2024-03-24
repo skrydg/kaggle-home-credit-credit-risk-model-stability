95b765996c298736f7b5054ccb6bc43a568573c4
skrydg <skrrydg@yandex.ru>
2024-03-24 15:01:55 +0400

    fix

---
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/merge_chunked_tables.py
@@ -0,0 +1,20 @@
+import numpy as np
+import polars as pl
+import gc
+
+from kaggle_home_credit_risk_model_stability.libs.input.dataset import Dataset
+
+class MergeChunkedTablesStep:        
+    def process_train_dataset(self, train_dataset_generator):
+        yield self.process(train_dataset_generator)
+        
+    def process_test_dataset(self, test_dataset_generator):
+        yield self.process(test_dataset_generator)
+    
+    def process(self, dataset_generator):
+        datasets = []
+        columns_info = None
+        for dataset, ci in dataset_generator:
+            datasets.append(dataset)
+            columns_info = ci
+        return pl.concat(datasets, how="vertical_relaxed"), columns_info
