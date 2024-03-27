efe6fe1db656e67a754448d50d8a732ca7a91845
skrydg <skrrydg@yandex.ru>
2024-03-27 19:58:38 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/lightgbm/__init__.py
+++ src/kaggle_home_credit_risk_model_stability/libs/lightgbm/__init__.py
@@ -1,4 +1,4 @@
 from .serializer import LightGbmDatasetSerializer
 from .dataset_creator import LightGbmDatasetCreator
-from .model import LightGbmModel
+from .model import LightGbmModel, WeeksKFold
 from .to_pandas import to_pandas
