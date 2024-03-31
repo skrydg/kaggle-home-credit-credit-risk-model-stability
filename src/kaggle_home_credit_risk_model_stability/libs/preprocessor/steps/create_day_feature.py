8428c9900756d06c74121e85243136c80ee5a038
skrydg <skrrydg@yandex.ru>
2024-03-31 18:28:50 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/create_day_feature.py
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/create_day_feature.py
@@ -4,11 +4,13 @@ import polars as pl
 from kaggle_home_credit_risk_model_stability.libs.input.dataset import Dataset
 
 class CreateDayFeatureStep:
-    def process_train_dataset(self, dataset, columns_info):
-        return self.process(dataset, columns_info)
+    def process_train_dataset(self, dataset_generator):
+        for dataset, columns_info in dataset_generator:
+          yield self.process(dataset, columns_info)
         
-    def process_test_dataset(self, dataset, columns_info):
-        return self.process(dataset, columns_info)
+    def process_test_dataset(self, dataset_generator):
+        for dataset, columns_info in dataset_generator:
+          yield self.process(dataset, columns_info)
     
     def process(self, dataset, columns_info):
         base = dataset.get_base()
