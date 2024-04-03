b372d60533dafb30ee14e98b1758c31e5d715f6c
skrydg <skrrydg@yandex.ru>
2024-04-04 01:11:19 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/feature_selection/correlation_groups.py
+++ src/kaggle_home_credit_risk_model_stability/libs/feature_selection/correlation_groups.py
@@ -1,5 +1,6 @@
 import polars as pl
 import numpy as np
+import scipy
 
 from collections import defaultdict
 
@@ -11,6 +12,32 @@ class CorrelationGroupsFeatureSelector:
         numerical_features = [feature for feature in features if dataframe[feature].dtype != pl.Enum]
         categorical_features = [feature for feature in features if dataframe[feature].dtype == pl.Enum]
 
+        return self.select_numerical(dataframe, numerical_features) + \
+              self.select_categorical(dataframe, categorical_features)
+    
+    def select_categorical(self, dataframe, categorical_features):
+        bad_mask = np.zeros(len(categorical_features), dtype=bool)
+        for feature1_index in len(categorical_features):
+            for feature2_index in len(feature1_index + 1, categorical_features):
+                feature1 = categorical_features[feature1_index]
+                feature2 = categorical_features[feature2_index]
+                
+                corr_coef = self.get_correlation_for_categorical_features(dataframe, feature1, feature2)
+                if (corr_coef > self.threshold):
+                    if (dataframe[feature1].n_unique() > dataframe[feature2].n_unique()):
+                        bad_mask[feature2_index] = True
+        return np.array(categorical_features)[~bad_mask]
+    
+    def get_correlation_for_categorical_features(self, dataframe, feature1, feature2):
+        cur_df = dataframe[[feature1, feature2]].with_columns(pl.lit(1).alias("const_1"))
+
+        pivot_df = cur_df.pivot(index=feature1, columns=feature2, values="const_1", aggregate_function="sum")
+        pivot_df = pivot_df.fill_null(0)
+        pivot_df = pivot_df.drop(feature1)
+
+        return scipy.stats.contingency.association(pivot_df.to_numpy(), method='pearson')
+
+    def select_numerical(self, dataframe, numerical_features):
         null_df = dataframe[numerical_features].select(pl.all().is_null())
         null_df = null_df.sum()
         null_array = sorted(list(zip(null_df.to_numpy().tolist()[0], null_df.columns)))
@@ -24,7 +51,7 @@ class CorrelationGroupsFeatureSelector:
                     break
             null_groups[cur_group].append(feature)
 
-        print("Count nulls:", sorted(list(null_groups.keys())))
+        print("Count nulls in numerical features:", sorted(list(null_groups.keys())))
         features_to_use = []
         for _, group in null_groups.items():
             if (len(group) == 1):
@@ -33,11 +60,11 @@ class CorrelationGroupsFeatureSelector:
             groups_by_correlation = self.group_columns_by_correlation(dataframe, group)            
             assert (np.sum([len(g) for g in groups_by_correlation]) == len(group))
             
-            print("groups_by_correlation: ", groups_by_correlation)
+            print("groups_by_correlation in numerical features: ", groups_by_correlation)
             for sub_group in groups_by_correlation:
                 features_to_use.append(self.get_most_various_feature(dataframe, sub_group))
 
-        return features_to_use + categorical_features
+        return features_to_use
 
     def group_columns_by_correlation(self, dataframe, features):
         groups = []
