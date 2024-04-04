13fa1cb097450e294f8ce7e71c04a625282e503b
skrydg <skrrydg@yandex.ru>
2024-04-04 11:06:27 +0400

    add table loader

--- src/kaggle_home_credit_risk_model_stability/libs/input/__init__.py
+++ src/kaggle_home_credit_risk_model_stability/libs/input/__init__.py
@@ -1,3 +1,4 @@
 from .data_loader import *
 from .dataset import *
 from .raw_table_info import RawTableInfo
+from .table_loader import TableLoader
---
+++ src/kaggle_home_credit_risk_model_stability/libs/input/table_loader.py
@@ -0,0 +1,15 @@
+import polars as pl
+
+from kaggle_home_credit_risk_model_stability.libs.env import Env
+from kaggle_home_credit_risk_model_stability.libs.input.data_loader import DataLoader
+
+class TableLoader:
+    def __init__(self, env: Env):
+        self.env = env
+        self.data_loader = DataLoader(env)
+
+    def load(self, table_name):
+        table_paths = self.data_loader._get_train_data_paths()[table_name]
+
+        chunks = [self.data_loader._read_file(path) for path in table_paths]
+        return pl.concat(chunks, how="vertical_relaxed")
