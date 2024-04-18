d5dc0006ffaf07b3fd1bd68c78186a7c154331a5
skrydg <skrrydg@yandex.ru>
2024-04-18 17:52:36 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/input/table_loader.py
+++ src/kaggle_home_credit_risk_model_stability/libs/input/table_loader.py
@@ -8,8 +8,8 @@ class TableLoader:
         self.env = env
         self.data_loader = DataLoader(env)
 
-    def load(self, table_name):
+    def load(self, table_name, filter=True):
         table_paths = self.data_loader._get_train_data_paths()[table_name]
 
-        chunks = [self.data_loader._read_file(path) for path in table_paths]
+        chunks = [self.data_loader._read_file(path).filter(filter) for path in table_paths]
         return pl.concat(chunks, how="vertical_relaxed")
