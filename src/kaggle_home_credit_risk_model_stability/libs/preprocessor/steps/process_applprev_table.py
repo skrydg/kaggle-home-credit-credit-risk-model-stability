bb61a94566731dc3cac21337e6ef8566e82ef861
skrydg <skrrydg@yandex.ru>
2024-04-24 15:38:20 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/process_applprev_table.py
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/process_applprev_table.py
@@ -22,9 +22,9 @@ class ProcessApplprevTableStep:
         table_rel = table.filter(pl.col("credtype_587L") == "REL")
         table_null = table.filter(pl.col("credtype_587L").is_null())
 
-        dataset.set(f"{self.table_name}_col", table_col)
-        dataset.set(f"{self.table_name}_cal", table_cal)
-        dataset.set(f"{self.table_name}_rel", table_rel)
-        dataset.set(f"{self.table_name}_null", table_null)
+        dataset.set(f"applprev_col_1", table_col)
+        dataset.set(f"applprev_cal_1", table_cal)
+        dataset.set(f"applprev_rel_1", table_rel)
+        dataset.set(f"applprev_null_1", table_null)
         dataset.delete(self.table_name)
         return dataset, columns_info
