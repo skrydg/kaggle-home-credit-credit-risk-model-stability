4900d0377b1dc470325a8c2e35db95781a407191
skrydg <skrrydg@yandex.ru>
2024-04-25 17:30:26 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/process_credit_bureau_a_1_table.py
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/process_credit_bureau_a_1_table.py
@@ -6,14 +6,25 @@ class ProcessCreditBureaua1TableStep:
     def __init__(self):
         self.table_name = "credit_bureau_a_1"
         self.service_columns = ["case_id", "num_group1"]
-        self.close_contracts_columns = ['annualeffectiverate_199L', 'classificationofcontr_400M', 'contractst_964M', 'credlmt_230A', 'dateofcredend_353D', 'dateofcredstart_181D', 'dateofrealrepmt_138D', 'dpdmax_757P', 'dpdmaxdatemonth_442T', 'financialinstitution_382M', 'instlamount_852A', 'interestrate_508L', 'lastupdate_388D', 'monthlyinstlamount_674A', 'nominalrate_498L', 'numberofinstls_229L', 'numberofoutstandinstls_520L', 'numberofoverdueinstlmax_1151L', 'numberofoverdueinstlmaxdat_148D', 'numberofoverdueinstls_834L', 'outstandingamount_354A', 'overdueamount_31A', 'overdueamountmax2_398A', 'overdueamountmax2date_1002D', 'overdueamountmax_35A', 'overdueamountmaxdatemonth_284T', 'periodicityofpmts_1102L', 'prolongationcount_1120L', 'purposeofcred_874M', 'residualamount_488A', 'totalamount_6A', 'subjectrole_93M']
-        self.close_contracts_columns.remove("financialinstitution_382M")
-        self.close_contracts_columns.remove("classificationofcontr_400M")
-        self.close_contracts_columns.remove("contractst_964M")
-        self.active_contracts_columns = ['annualeffectiverate_63L', 'classificationofcontr_13M', 'contractst_545M', 'contractsum_5085717L', 'credlmt_935A', 'dateofcredend_289D', 'dateofcredstart_739D', 'dpdmax_139P', 'dpdmaxdatemonth_89T', 'financialinstitution_591M', 'instlamount_768A', 'lastupdate_1112D', 'monthlyinstlamount_332A', 'nominalrate_281L', 'numberofinstls_320L', 'numberofoutstandinstls_59L', 'numberofoverdueinstlmax_1039L', 'numberofoverdueinstlmaxdat_641D', 'numberofoverdueinstls_725L', 'outstandingamount_362A', 'overdueamount_659A', 'overdueamountmax2_14A', 'overdueamountmax2date_1142D', 'overdueamountmax_155A', 'overdueamountmaxdatemonth_365T', 'periodicityofpmts_837L', 'prolongationcount_599L', 'purposeofcred_426M', 'residualamount_856A', 'subjectrole_182M', 'totalamount_996A']
-        self.active_contracts_columns.remove("financialinstitution_591M")
-        self.active_contracts_columns.remove("classificationofcontr_13M")
-        self.active_contracts_columns.remove("contractst_545M")
+
+        self.config = {
+            "active": {
+                "mask_column": "dateofcredstart_739D",
+                "financialinstitution_column": "financialinstitution_591M",
+                "columns": ['annualeffectiverate_63L', 'contractsum_5085717L', 'credlmt_935A', 'dateofcredend_289D', 'dateofcredstart_739D', 'dpdmax_139P', 'dpdmaxdatemonth_89T', 'instlamount_768A', 'lastupdate_1112D', 'monthlyinstlamount_332A', 'nominalrate_281L', 'numberofinstls_320L', 'numberofoutstandinstls_59L', 'numberofoverdueinstlmax_1039L', 'numberofoverdueinstlmaxdat_641D', 'numberofoverdueinstls_725L', 'outstandingamount_362A', 'overdueamount_659A', 'overdueamountmax2_14A', 'overdueamountmax2date_1142D', 'overdueamountmax_155A', 'overdueamountmaxdatemonth_365T', 'periodicityofpmts_837L', 'prolongationcount_599L', 'purposeofcred_426M', 'residualamount_856A', 'subjectrole_182M', 'totalamount_996A']
+            },
+            "close": {
+                "mask_column": "dateofcredstart_181D",
+                "financialinstitution_column": "financialinstitution_382M",
+                "columns": ['annualeffectiverate_199L', 'credlmt_230A', 'dateofcredend_353D', 'dateofcredstart_181D', 'dateofrealrepmt_138D', 'dpdmax_757P', 'dpdmaxdatemonth_442T', 'instlamount_852A', 'interestrate_508L', 'lastupdate_388D', 'monthlyinstlamount_674A', 'nominalrate_498L', 'numberofinstls_229L', 'numberofoutstandinstls_520L', 'numberofoverdueinstlmax_1151L', 'numberofoverdueinstlmaxdat_148D', 'numberofoverdueinstls_834L', 'outstandingamount_354A', 'overdueamount_31A', 'overdueamountmax2_398A', 'overdueamountmax2date_1002D', 'overdueamountmax_35A', 'overdueamountmaxdatemonth_284T', 'periodicityofpmts_1102L', 'prolongationcount_1120L', 'purposeofcred_874M', 'residualamount_488A', 'totalamount_6A', 'subjectrole_93M']
+            }
+        }
+
+        self.finantial_institutions = {
+            "active": ['0d39f5db', 'P51_123_23', 'P102_97_118', 'd6a7d943', '50babcd4', 'P150_136_157', 'P133_127_114', 'b619fa46', 'Home Credit', 'P204_66_73'],
+            "close": ['dcb42d2c', 'e9bfdb5c', 'a4f0ab55', '952e9882', 'P40_25_35', '50babcd4', '9a93e20f', 'd6a7d943', 'P204_66_73', 'P40_52_135', 'b619fa46', 'P150_136_157', 'P133_127_114', 'Home Credit'],
+        }
+
         self.other_columns = [
             'totaldebtoverduevalue_178A', 
             'totaldebtoverduevalue_718A',
@@ -38,20 +49,27 @@ class ProcessCreditBureaua1TableStep:
     def process(self, dataset, columns_info):
         table = dataset.get_table(self.table_name)
 
-        # active contracts
-        column = "dateofcredstart_739D"
-        mask = table[column].is_not_null()
-        assert(column in self.active_contracts_columns)
-        dataset.set("credit_bureau_a_active_contracts_1", table.filter(mask)[self.active_contracts_columns + self.service_columns])
-
-        # close contracts
-        column = "dateofcredstart_181D"
-        mask = table[column].is_not_null()
-        assert(column in self.close_contracts_columns)
-        dataset.set("credit_bureau_a_close_contracts_1", table.filter(mask)[self.close_contracts_columns + self.service_columns])
-
+        for contract_type in self.config.keys():
+            dataset = self.process_contract(contract_type, table, dataset)
+            
         # other
-        dataset.set("credit_bureau_a_other_1", table[self.other_columns + self.service_columns])
+        dataset.set(f"other_{self.table_name}", table[self.other_columns + self.service_columns])
 
         dataset.delete(self.table_name)
         return dataset, columns_info
+    
+    def process_contracts(self, contract_type, table, dataset):
+        mask_column = self.config[contract_type]["mask_column"]
+        mask = table[mask_column].is_not_null()
+        contracts = table.filter(mask)
+
+        financialinstitution_column = self.config[contract_type]["financialinstitution_column"]
+        columns = self.config[contract_type]["columns"]
+
+        for finantial_institution in self.finantial_institutions[contract_type]:
+            finantial_institution_table = contracts.filter(pl.col(financialinstitution_column) == finantial_institution)
+            table_name = f"{contract_type}_{finantial_institution}_{self.table_name}"
+            dataset.set(table_name, finantial_institution_table[columns + self.service_columns])
+            print(f"Generate new table: {table_name}")
+
+        return dataset
