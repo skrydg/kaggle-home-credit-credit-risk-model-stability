24bfff6b7f5864f1f7bdf6675b74b9f4e0623754
skrydg <skrrydg@yandex.ru>
2024-04-26 12:29:58 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/process_credit_bureau_a_1_table.py
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/process_credit_bureau_a_1_table.py
@@ -8,11 +8,12 @@ import polars as pl
 #
 #
 class ProcessCreditBureaua1TableStep:
-    def __init__(self):
+    def __init__(self, config = None, finantial_institutions = None):
         self.table_name = "credit_bureau_a_1"
         self.service_columns = ["case_id", "num_group1"]
 
-        self.config = {
+        if config is None:
+          config = {
             "active": {
                 "mask_column": "dateofcredstart_739D",
                 "financialinstitution_column": "financialinstitution_591M",
@@ -21,24 +22,23 @@ class ProcessCreditBureaua1TableStep:
                     'contractsum_5085717L', 'credlmt_935A', 'dateofcredend_289D', 'dateofcredstart_739D', 'dpdmaxdatemonth_89T', 'instlamount_768A', 'lastupdate_1112D', 'monthlyinstlamount_332A', 'numberofinstls_320L', 'numberofoverdueinstlmax_1039L', 'outstandingamount_362A', 'overdueamount_659A', 'overdueamountmax2_14A', 'overdueamountmax2date_1142D', 'overdueamountmax_155A', 'overdueamountmaxdatemonth_365T', 'periodicityofpmts_837L', 'purposeofcred_426M', 'residualamount_856A', 'subjectrole_182M', 'totalamount_996A'
                 ]
             },
-            # "close": {
-            #     "mask_column": "dateofcredstart_181D",
-            #     "financialinstitution_column": "financialinstitution_382M",
-            #     "columns": [
-            #         # 'interestrate_508L', 'annualeffectiverate_199L', 'dateofrealrepmt_138D', 'lastupdate_388D', 'numberofoverdueinstlmaxdat_148D', 'prolongationcount_1120L',
-            #         'credlmt_230A', 'dateofcredend_353D', 'dateofcredstart_181D', 'dpdmax_757P', 'dpdmaxdatemonth_442T', 'instlamount_852A', 'monthlyinstlamount_674A', 'nominalrate_498L', 'numberofinstls_229L', 'numberofoutstandinstls_520L', 'numberofoverdueinstlmax_1151L', 'numberofoverdueinstls_834L', 'outstandingamount_354A', 'overdueamount_31A', 'overdueamountmax2_398A', 'overdueamountmax2date_1002D', 'overdueamountmax_35A', 'overdueamountmaxdatemonth_284T', 'periodicityofpmts_1102L', 'purposeofcred_874M', 'residualamount_488A', 'totalamount_6A', 'subjectrole_93M'
-            #     ]
-            # }
-        }
+            "close": {
+                "mask_column": "dateofcredstart_181D",
+                "financialinstitution_column": "financialinstitution_382M",
+                "columns": [
+                    # 'interestrate_508L', 'annualeffectiverate_199L', 'dateofrealrepmt_138D', 'lastupdate_388D', 'numberofoverdueinstlmaxdat_148D', 'prolongationcount_1120L',
+                    'credlmt_230A', 'dateofcredend_353D', 'dateofcredstart_181D', 'dpdmax_757P', 'dpdmaxdatemonth_442T', 'instlamount_852A', 'monthlyinstlamount_674A', 'nominalrate_498L', 'numberofinstls_229L', 'numberofoutstandinstls_520L', 'numberofoverdueinstlmax_1151L', 'numberofoverdueinstls_834L', 'outstandingamount_354A', 'overdueamount_31A', 'overdueamountmax2_398A', 'overdueamountmax2date_1002D', 'overdueamountmax_35A', 'overdueamountmaxdatemonth_284T', 'periodicityofpmts_1102L', 'purposeofcred_874M', 'residualamount_488A', 'totalamount_6A', 'subjectrole_93M'
+                ]
+            }
+          }
 
-        self.finantial_institutions = {
-            "active": ['Home Credit']
-        }
-
-        # self.finantial_institutions = {
-        #     "active": ['P150_136_157', 'P133_127_114', 'b619fa46', 'Home Credit', 'P204_66_73'],
-        #     "close": ['P40_52_135', 'b619fa46', 'P150_136_157', 'P133_127_114', 'Home Credit']
-        # }
+        if finantial_institutions is None:
+          finantial_institutions = {
+              "active": ['P150_136_157', 'P133_127_114', 'b619fa46', 'Home Credit', 'P204_66_73'],
+              "close": ['P40_52_135', 'b619fa46', 'P150_136_157', 'P133_127_114', 'Home Credit']
+          }
+        self.config = config
+        self.finantial_institutions = finantial_institutions
 
         # self.other_columns = [
         #     'totaldebtoverduevalue_178A', 
