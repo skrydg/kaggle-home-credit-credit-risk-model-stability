9d1575c6504c64f481b898f863f68844f276e98a
skrydg <skrrydg@yandex.ru>
2024-04-27 19:52:30 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/reduce_dimention_for_categorical_features.py
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/reduce_dimention_for_categorical_features.py
@@ -9,38 +9,31 @@ class ReduceDimentionForCategoricalFeaturesStep:
         self.feature_to_values = {}
 
     def process_train_dataset(self, dataframe_generator):
-        dataset, columns_info = next(dataframe_generator)
-        self.set_values(columns_info)
-        yield self.process(dataset, columns_info)
-
-        for dataset, columns_info in dataframe_generator:
-            yield self.process(dataset, columns_info)
+        dataframe, columns_info = next(dataframe_generator)
+        self.set_values(dataframe)
+        yield self.process(dataframe, columns_info)
 
     def process_test_dataset(self, dataframe_generator):
-        for dataset, columns_info in dataframe_generator:
-            yield self.process(dataset, columns_info)
+        for dataframe, columns_info in dataframe_generator:
+            yield self.process(dataframe, columns_info)
 
-    def process(self, dataset, columns_info):
-        for table_name, table in dataset.get_tables():
-            for feature in table.columns:
-                if ("CATEGORICAL" in columns_info.get_labels(feature)) and ("RAW" in columns_info.get_labels(feature)):
-                    table = table.with_columns(
-                        table[feature]
-                            .cast(pl.String)
-                            .set(~table[feature].is_in(self.feature_to_values[feature]), "__OTHER__")
-                            .cast(pl.Enum(self.feature_to_values[feature]))
+    def process(self, table, columns_info):
+        for feature in table.columns:
+            if (table[feature].dtype == pl.Enum):
+                table = table.with_columns(
+                    table[feature]
+                        .cast(pl.String)
+                        .set(~table[feature].is_in(self.feature_to_values[feature]), "__OTHER__")
+                        .cast(pl.Enum(self.feature_to_values[feature]))
                     )
-            dataset.set(table_name, table)
-        return dataset, columns_info
+        return table, columns_info
 
-    def set_values(self, columns_info):
-        raw_tables_info = columns_info.get_raw_tables_info()
-        for table_name in raw_tables_info.keys():
-            for feature in raw_tables_info[table_name].get_columns():
-                if ("CATEGORICAL" in columns_info.get_labels(feature)) and ("RAW" in columns_info.get_labels(feature)):
-                    value_counts = raw_tables_info[table_name].get_value_counts(feature)
-                    total_count = value_counts.sum()["count"][0]
-                    threashold = self.non_significant_treashold * total_count
-                    values = value_counts.filter(pl.col("count") > threashold)[feature].unique().to_numpy().tolist()
-                    values = sorted(np.unique(values + ["__OTHER__", "__NULL__", "__UNKNOWN__"]))
-                    self.feature_to_values[feature] = values
+    def set_values(self, dataframe):
+        for feature in dataframe.columns:
+            if (dataframe[feature].dtype == pl.Enum):
+                value_counts = dataframe[feature].value_counts()
+                total_count = value_counts.sum()["count"][0]
+                threashold = self.non_significant_treashold * total_count
+                values = value_counts.filter(pl.col("count") > threashold)[feature].unique().to_numpy().tolist()
+                values = sorted(np.unique(values + ["__OTHER__", "__NULL__", "__UNKNOWN__"]))
+                self.feature_to_values[feature] = values
