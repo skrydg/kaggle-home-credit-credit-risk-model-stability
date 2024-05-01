09b7275c5c41114296c3a0306fe551b9459a7301
skrydg <skrrydg@yandex.ru>
2024-05-01 20:41:58 +0400

    fix sample

--- src/kaggle_home_credit_risk_model_stability/libs/sample.py
+++ src/kaggle_home_credit_risk_model_stability/libs/sample.py
@@ -1,5 +1,9 @@
 import polars as pl
 
-def sample(dataframe, fraction):
-    return dataframe.group_by("WEEK_NUM") \
-                    .map_groups(lambda df: df.group_by("target").map_groups(lambda df: df.sample(fraction=0.1)))
+def sample(dataframe, false_target_fraction, true_target_fraction):
+    return dataframe.group_by("WEEK_NUM").map_groups(
+              lambda df: pl.concat([
+                    df.filter(pl.col("target") == 0).sample(fraction = false_target_fraction),
+                    df.filter(pl.col("target") == 1).sample(fraction = true_target_fraction)
+                ], how="vertical_relaxed")
+            )
