3febf65c26c8e13228300b1f29f665c32c90d0df
skrydg <skrrydg@yandex.ru>
2024-05-02 11:28:42 +0400

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/nearest_to_target_ensemble_model.py
+++ src/kaggle_home_credit_risk_model_stability/libs/nearest_to_target_ensemble_model.py
@@ -4,9 +4,7 @@ class NearestToTargetEnsembleModel:
     def __init__(self, target):
         self.target = target
 
-    def predict(self, dataframe):
-        assert(dataframe.shape[0] == self.target.shape[0])
-        np_array = dataframe
-        np_array = np_array - self.target[:, np.newaxis]
-        nearest_to_target_index = np_array.transpose().argmin(axis=0)
-        return np_array[np.arange(nearest_to_target_index.shape[0]), nearest_to_target_index]
+    def predict(self, X):
+        assert(X.shape[0] == self.target.shape[0])
+        nearest_to_target_index = np.absolute((X - self.target[:, np.newaxis]).transpose()).argmin(axis=0)
+        return X[np.arange(nearest_to_target_index.shape[0]), nearest_to_target_index]
