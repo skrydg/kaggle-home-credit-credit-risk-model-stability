f04f3ada620b04295f7e4ebe88c8a2753fb83d25
skrydg <skrrydg@yandex.ru>
2024-05-06 12:08:31 +0400

    skip categorical features

--- src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/reduce_memory_usage_for_dataframe.py
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/reduce_memory_usage_for_dataframe.py
@@ -18,7 +18,10 @@ class ReduceMemoryUsageForDataFrameStep:
         for column in df.columns:
             column_type = df[column].dtype
 
-            if (column_type != pl.Enum) and (column_type != pl.String) and (column_type != pl.Categorical):
+            if (column_type != pl.Enum) and \
+              (column_type != pl.String) and \
+              (column_type != pl.Categorical) and \
+              ("SERVICE" not in columns_info.get_labels(column)):
                 c_min = df[column].min()
                 c_max = df[column].max()
                 if (c_min is None) or (c_max is None):
