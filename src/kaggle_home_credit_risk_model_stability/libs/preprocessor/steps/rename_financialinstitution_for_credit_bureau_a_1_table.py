f7b1841edbd9e27e0ff75b8eab41578b3a639575
skrydg <skrrydg@yandex.ru>
2024-05-07 10:41:36 +0400

    add rename_financialinstitution_for_credit_bureau_a_1_table

---
+++ src/kaggle_home_credit_risk_model_stability/libs/preprocessor/steps/rename_financialinstitution_for_credit_bureau_a_1_table.py
@@ -0,0 +1,32 @@
+import numpy as np
+import polars as pl
+
+
+class RenameFinancialInstitutionForCreditBureauA1TableStep:
+    def __init__(self):
+        self.table_name = "credit_bureau_a_1"
+
+    def process_train_dataset(self, dataset_generator):
+        for dataset, columns_info in dataset_generator:
+            yield self.process(dataset, columns_info)
+    
+    def process_test_dataset(self, dataset_generator):
+        for dataset, columns_info in dataset_generator:
+            yield self.process(dataset, columns_info)
+
+    def process(self, dataset, columns_info):
+        table = dataset.get_table(self.table_name)
+
+        for column in ["financialinstitution_382M", "financialinstitution_591M"]:
+          table = table.with_columns(table[column].replace({
+              "7e4feb1b": "66b2baaa",
+              "cc2c2610": "55b002a9",
+              "9325d851": "0d39f5db",
+              "b619fa46": "P204_66_73",
+              "71de340a": "cb830fec",
+              "952e9882": "P40_25_35"
+          }))
+
+        dataset.set(self.table_name, table)
+        return dataset, columns_info
+    
