aa92e53e86720528d302c9094a7f94ccaaf88e23
skrydg <skrrydg@yandex.ru>
2024-05-08 10:47:50 +0400

    add is_test

--- src/kaggle_home_credit_risk_model_stability/libs/input/table_loader.py
+++ src/kaggle_home_credit_risk_model_stability/libs/input/table_loader.py
@@ -8,8 +8,11 @@ class TableLoader:
         self.env = env
         self.data_loader = DataLoader(env)
 
-    def load(self, table_name, filter=True):
-        table_paths = self.data_loader._get_train_data_paths()[table_name]
+    def load(self, table_name, filter=True, is_test=False):
+        if is_test:
+            table_paths = self.data_loader._get_test_data_paths()[table_name]
+        else:
+            table_paths = self.data_loader._get_train_data_paths()[table_name]
 
         chunks = [self.data_loader._read_file(path).filter(filter) for path in table_paths]
         return pl.concat(chunks, how="vertical_relaxed")
