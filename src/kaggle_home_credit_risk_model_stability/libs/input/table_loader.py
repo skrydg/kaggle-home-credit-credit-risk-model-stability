28f90e7ae61832ffc3106dae32455fac4e6facaf
skrydg <skrrydg@yandex.ru>
2024-05-10 23:12:37 +0400

    add columns in TableLoader

--- src/kaggle_home_credit_risk_model_stability/libs/input/table_loader.py
+++ src/kaggle_home_credit_risk_model_stability/libs/input/table_loader.py
@@ -8,11 +8,14 @@ class TableLoader:
         self.env = env
         self.data_loader = DataLoader(env)
 
-    def load(self, table_name, filter=True, is_test=False):
+    def load(self, table_name, filter=True, is_test=False, columns=None):
         if is_test:
             table_paths = self.data_loader._get_test_data_paths()[table_name]
         else:
             table_paths = self.data_loader._get_train_data_paths()[table_name]
 
-        chunks = [self.data_loader._read_file(path).filter(filter) for path in table_paths]
+        if columns is None:
+            chunks = [pl.read_parquet(path).filter(filter) for path in table_paths]
+        else:
+            chunks = [pl.read_parquet(path, columns=columns).filter(filter) for path in table_paths]
         return pl.concat(chunks, how="vertical_relaxed")
