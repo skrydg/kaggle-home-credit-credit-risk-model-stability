80ff36d5a6fa1b40736b029ebb170cd291fe2eea
skrydg <skrrydg@yandex.ru>
2024-05-18 17:00:46 +0900

    fix

--- src/kaggle_home_credit_risk_model_stability/libs/catboost/pre_trained_model.py
+++ src/kaggle_home_credit_risk_model_stability/libs/catboost/pre_trained_model.py
@@ -3,4 +3,4 @@ class PreTrainedCatboostModel:
         self.model = model
 
     def predict(self, *args, **kwargs):
-        return self.model.predict(*args, **kwargs, prediction_type="Probability")[:, 1]
+        return self.model.predict_proba(*args, **kwargs)[:, 1]
